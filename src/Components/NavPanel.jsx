dc2438a590805ede13b0ceb20bcff55826afb0b9
rttn-Mango <khimmanga445@gmail.com>
2023-11-13 02:02:39 +0800

    Changes: changed cv to resume

--- src/Components/NavPanel.jsx
+++ src/Components/NavPanel.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import CV from '../../public/CV.pdf'
+import resume from '../../public/Resume.pdf'
 
 export default function NavPanel({setShowNav}){
     return(
@@ -9,7 +9,7 @@ export default function NavPanel({setShowNav}){
                 <li><Link to='/about' title='About Me' onClick={()=> {setShowNav(false)}}><span>A</span>bout</Link></li>
                 <li><Link to='/works' title='Personal Projects' onClick={()=> {setShowNav(false)}}><span>P</span>rojects</Link></li>
                 <li><Link to='/contact' title='Contact Details' onClick={()=> {setShowNav(false)}}><span>C</span>ontacts</Link></li>
-                <li><a href={CV} className="btn" download="Manga, Kim Oliver - CV">Download CV</a></li>
+                <li><a href={resume} className="btn" download="Manga, Kim Oliver - Resume">Download Resume</a></li>
             </ul>
         </>
     )
