8c7cdc7475b3360a34f7ebb4cbcc3e7749628d73
rttn-Mango <khimmanga445@gmail.com>
2023-11-15 04:24:24 +0800

    Changes: added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter} from 'react-router-dom';
 import ScrollToTop from './hooks/ScrollToTop.jsx';
-import './Styles/index.css'
+import './Styles/index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop/>
       <App />
+      <Analytics/>
     </BrowserRouter>
   </React.StrictMode>,
 )
