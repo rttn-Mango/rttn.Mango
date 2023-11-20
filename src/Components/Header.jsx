fd544b89e725b4ddaaa2d606c38e51afeb8f79f3
rttn-Mango <khimmanga445@gmail.com>
2023-11-21 01:09:16 +0800

    Changes: added dl btn on header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { motion } from "framer-motion";
 import Close from "../svg/Close";
 import Burger from "../svg/Burger";
+import Download from '../assets/Download.svg'
+import resume from '../../public/Resume.pdf'
 
 export default function Header({setShowNav, showNav}){
     const logoAnim = {
@@ -42,6 +44,7 @@ export default function Header({setShowNav, showNav}){
                         <motion.span variants={navPagesAnim}><Link to="/about" smooth title="Who am I?">About</Link></motion.span>
                         <motion.span variants={navPagesAnim}><Link to="/works" smooth title="Personal Projects">Works</Link></motion.span>
                         <motion.span variants={navPagesAnim}><Link to="/contact" smooth title="Send me a Message">Contact</Link></motion.span>
+                        <motion.span variants={navPagesAnim}><a href={resume} className="btn" download="Manga, Kim Oliver - Resume">Resume <img src={Download} alt="Download Icon" draggable="false" width={30} height={30}/></a></motion.span>
                     </motion.li>
 
                     <motion.li className="header__mobile-menu" initial="initial" animate="animate" transition={{staggerChildren: .3}}>
--- src/Components/NavPanel.jsx
+++ src/Components/NavPanel.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import resume from '../../public/Resume.pdf'
+import Download from '../assets/Download.svg'
 
 export default function NavPanel({setShowNav}){
     return(
@@ -9,7 +10,7 @@ export default function NavPanel({setShowNav}){
                 <li><Link to='/about' title='About Me' onClick={()=> {setShowNav(false)}}><span>A</span>bout</Link></li>
                 <li><Link to='/works' title='Personal Projects' onClick={()=> {setShowNav(false)}}><span>W</span>orks</Link></li>
                 <li><Link to='/contact' title='Contact Details' onClick={()=> {setShowNav(false)}}><span>C</span>ontact</Link></li>
-                <li><a href={resume} className="btn" download="Manga, Kim Oliver - Resume">Download Resume</a></li>
+                <li><a href={resume} className="btn" download="Manga, Kim Oliver - Resume">Resume <img src={Download} alt="Download Icon" draggable="false" width={30} height={30}/></a></li>
             </ul>
         </>
     )
