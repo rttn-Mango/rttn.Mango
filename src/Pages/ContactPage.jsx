662efe93fd82fb2a30dcaa6283b581be68ee8b66
rttn-Mango <khimmanga445@gmail.com>
2024-01-14 01:25:23 +0800

    Changes: minor updates on contact page

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,30 +1,10 @@
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 //Image
 import right from '../assets/Right.svg'
 
 export default function ContactPage() {
-    const textAnim = {
-        initial: {opacity: 0},
-        animate: {
-            opacity: 1,
-            transition: {
-                duration: .5,
-                ease: [0.45, 0, 0.55, 1]
-            }
-        }
-    }
-
-    const subHeadingAnim = {
-        initial: {y: 70, opacity: 0},
-        animate: {
-            y: 0,
-            opacity: 1,
-
-        }
-    }
 
     //To change document title once the Component mounts
     useEffect(() => {
@@ -35,7 +15,7 @@ export default function ContactPage() {
     return(
         
         <main className="contact-page">
-            <motion.h1 variants={textAnim} initial="initial" whileInView='animate'>Don&apos;t Hesitate to <span>Reach Out</span></motion.h1>
+            <h1 >Don&apos;t Hesitate to <span>Reach Out</span></h1>
 
             <section className="contact-page__container">
                 <form action="https://formsubmit.co/d04bc60964151e048879c5cd4e2a437f" method="POST" className="contact-page__form">
@@ -43,37 +23,35 @@ export default function ContactPage() {
                     {/*To disable captcha when sending message */}
                     <input type="hidden" name="_captcha" value='false'/>
 
-                    <motion.div variants={textAnim} initial="initial" whileInView='animate' className="form-group">
+                    <div className="form-group">
                         <label htmlFor="name">What should I call you<span>?</span></label>
                         <input id="name" name="name" type="text" placeholder="Ipsum Dolor" required/>
-                    </motion.div>
+                    </div>
 
-                    <motion.div variants={textAnim} initial="initial" whileInView='animate' className="form-group">
+                    <div className="form-group">
                         <label htmlFor="email">Your Email Address<span>?</span></label>
                         <input id="email" name="email" type="email" placeholder="Lorem-Ipsum@email.com" required/>
-                    </motion.div>
+                    </div>
 
-                    <motion.div variants={textAnim} initial="initial" whileInView='animate' className="form-group">
+                    <div className="form-group">
                         <label htmlFor="message">What&apos;s your question<span>?</span></label>
                         <textarea name="message" id="message" placeholder="Hello, I'd like to ask about..." required/>
-                    </motion.div>
+                    </div>
 
-                    <motion.button variants={textAnim} initial="initial" whileInView='animate' type="submit">Forward <img src={right} alt="right arrow" draggable="false" width={30} height={30}/></motion.button>
+                    <button type="submit">Forward <img src={right} alt="right arrow" draggable="false" width={30} height={30}/></button>
                 </form>
 
                 <section className="contact-page__links">
-                    <motion.h2 variants={subHeadingAnim} initial="initial" whileInView='animate'>Socials</motion.h2>
-                    <motion.div variants={textAnim} initial="initial" whileInView='animate' className="contact-page__links--socials">
-                        <Link to='https://github.com/rttn-Mango' title="Github Profile">Github</Link>
+                    <h2 >Socials</h2>
+                    <div className="contact-page__links--socials">
                         <Link to='www.linkedin.com/in/rttn-mango' title="LinkedIn Profile">LinkedIn</Link>
-                        <Link to='https://www.facebook.com/kiolma09' title="Facebook Profile">Facebook</Link>
+                        <Link to='https://github.com/rttn-Mango' title="Github Profile">Github</Link>
                         <Link to='https://www.instagram.com/rttn.mango/' title="Instagram Profile">Instagram</Link>
-                    </motion.div>
+                    </div>
 
                     <div className="contact-page__links--contacts">
-                        <motion.h2 variants={subHeadingAnim} initial="initial" whileInView='animate'>Contacts</motion.h2>
-                        <motion.p variants={textAnim} initial="initial" whileInView='animate'>+63 945 - 256 - 7613</motion.p>
-                        <motion.p variants={textAnim} initial="initial" whileInView='animate'>khimmanga445@gmail.com</motion.p>
+                        <h2 >Email</h2>
+                        <p>kim.oliver.manga@gmail.com</p>
                     </div>
                 </section>
             </section>
