eddbc3b5e34f09990ad2c09d4565ab2b1e018366
rttn-Mango <khimmanga445@gmail.com>
2024-02-03 00:59:57 +0800

    Bugfix: image not appearing when hovering on my name

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ import { Analytics } from '@vercel/analytics/react';
 
 //Icon
 import TiltedArrow from "./svg/TiltedArrow";
+import dp from '/dp.webp'
 
 function App() {
   const {loading, setLoading} = useLoadingContext();
@@ -160,7 +161,7 @@ function App() {
 
         <div className="gradient-mouse-follower" aria-hidden="true" style={{display: window.innerWidth < 1440 && 'none'}}></div>
         <div className="hover-mouse-follower" aria-hidden="true">
-          {hoverContent.elementToRender === 'img' && <img src="/public/dp.webp" alt="image of me" draggable="false" height={25} width={25}/>}
+          {hoverContent.elementToRender === 'img' && <img src={dp} alt="image of me" draggable="false" height={25} width={25}/>}
           {hoverContent.elementToRender !== null && hoverContent.elementToRender.includes('link') && <p className="minify-hover">Visit {hoverContent.elementToRender === 'minify-link' ? 'minify' : 'shortly'} <TiltedArrow/> </p>}
           {hoverContent.elementToRender === 'resume' && <span className="resume-hover">Download resume <TiltedArrow/></span>}
           {hoverContent.elementToRender !== null && hoverContent.elementToRender.includes('awwwards') && <p className="resume-hover">{hoverContent.elementToRender.includes('vucko') ? 'Vucko' : hoverContent.elementToRender.includes('huy') ? 'Huy Phan' : 'Bright Studios'} awwwards <TiltedArrow/></p>}
